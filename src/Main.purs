27d15a62b94002c9211f1c83db82452f79deeaa2
Jacob Alford - MBP <jacob.alford@me.com>
2022-03-14 18:24:33 -0600

    Add TODO

--- src/Main.purs
+++ src/Main.purs
@@ -4,7 +4,22 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Console (log)
+{--
 
+########### TODO ###########
+* --Create Rest.FetchMethods--
+* --Create API Structure--
+* Create RemoteData package
+* Add API functions
+  * Stocks
+  * Crypto
+  * Commodities
+* Test API with CLI
+* Add Halogen
+  * Test run
+* Finish TODO List with items from dashboard-persistence api
+
+-}
 main :: Effect Unit
 main = do
   log "🍝"
